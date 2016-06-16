a64c539463433d7621958555ed08b780320372d6
bournerider <logan.washbourne@okstate.edu>
2016-06-15 20:56:30 -0500

    Player move and obstacles.
    Player can move left and right, and can fall.
    Player also has obstacle detection.

--- SideScrolla/Assets/Scripts/Player.cs
+++ SideScrolla/Assets/Scripts/Player.cs
@@ -5,11 +5,25 @@ using System.Collections;
 [RequireComponent (typeof (Controller2D))]
 
 public class Player : MonoBehaviour {
+    float moveSpeed = 6;
+    Vector3 velocity;
+    float gravity = -20;
+
 
     Controller2D controller;
     
 	void Start()
     {
         controller = GetComponent<Controller2D> ();
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
     }
 }
